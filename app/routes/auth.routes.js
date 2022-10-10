51b2444cb335256d5ee79c4af83e98e46ed0c17b
Jawad <contact.syedjawad@gmail.com>
2022-10-10 18:20:31 +0500

    api's

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { auth } = require('../controller/');
 const app = express();
 
-app.post('/sign-up',auth.lol)
+app.post('/sign-up',auth.signUp)
+app.post('/sign-in',auth.signIn)
+app.post('/sign-out',auth.signOut)
 
 module.exports = app;
